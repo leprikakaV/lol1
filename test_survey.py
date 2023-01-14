7634af2b626dd64b643ff441eb979e5104e76937
Lepri <leprikaka@gmail.com>
2023-01-14 18:49:16 +0300

    v1

--- test_survey.py
+++ test_survey.py
@@ -3,12 +3,24 @@ from survey import AnonymousSurvey
 class TestAnonmyoussSurvey(unittest.TestCase):
     """ТЕсты для класса AnonymousSurvey"""
 
+    def setUp(self) -> None:
+        """"Создание опроса и набора ответов ддля всех тестовых методов"""
+        question = "What"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ["English", "Spanish", "Mandarine"]
+
     def test_store_single_response(self):
         """Проверяет, что один ответ сохранен правильно"""
-        question = "What language?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response("English")
-        self.assertIn("English", my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn("English", self.my_survey.responses)
+
+    def test_store_three_response(self):
+        """"Проверяет, что три ответа были сохранены правильно."""
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 if __name__ == "__main__":
     unittest.main()
